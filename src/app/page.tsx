930f11a222ed88e00df27ac66d26ec9d52f0f9d4
ashish-pro <ashishbit017@outlook.com>
2023-10-28 19:41:35 +0530

    new-arrival component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Hero } from '@/components/Hero'
 import Features from '@/components/Features'
+import NewArrival from '@/components/NewArrival'
 
 export default function Home() {
   return (
     <main>
       < Hero />
       < Features />
+      <NewArrival />
     </main>
   )
 }
